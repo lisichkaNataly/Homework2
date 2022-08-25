7feca4df8db061ddb18f3f37c023a67f84297d39
Izmaylova <Natalyatf@yandex.ru>
2022-08-25 23:41:39 +0500

    домашнее задание - 2

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,23 @@ public class Main {
         paper = paper - 7639;
         System.out.println(paper);
 
+        // Задание 4
+        var friend = 19;
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        // Задание 5
+        var frog = 3.5;
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+
 
 
 
